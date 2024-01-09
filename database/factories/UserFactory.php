63d5120af5053e1592ff5a80bebddd87eb28e7ae
Alaa Elhadi <alaaelhadi28@gmail.com>
2024-01-09 09:53:16 +0200

    Session15

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
             'mobile' => fake()->unique()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'expired' => 0,
             'remember_token' => Str::random(10),
         ];
     }
